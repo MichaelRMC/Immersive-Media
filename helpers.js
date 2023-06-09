b8a790cc86f00c3520208d2228e7cd5ea4baf497
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-06-09 00:56:14 -0400

    Change to Falso.js

--- helpers.js
+++ helpers.js
@@ -1,18 +1,16 @@
-const { readFileSync, writeFileSync } = require( "node:fs" );
+const { readFileSync, writeFileSync } = require("node:fs");
 
 function readJSONFile(path, fileName) {
-	const collection = readFileSync( `${path}/${fileName}`, "utf8", );
-	return collection ? JSON.parse(collection) : [];
+  const collection = readFileSync(`${path}/${fileName}`, "utf8");
+  return collection ? JSON.parse(collection) : [];
 }
 
-function writeJSONFile( path, fileName, data )
-{
-	data = JSON.stringify( data );
-	return writeFileSync( `${ path }/${ fileName }`, data, { ecoding: "utf-8" } );
+function writeJSONFile(path, fileName, data) {
+  data = JSON.stringify(data);
+  return writeFileSync(`${path}/${fileName}`, data, { encoding: "utf-8" });
 }
-	
 
 module.exports = {
-	readJSONFile,
-	writeJSONFile
+  readJSONFile,
+  writeJSONFile,
 };
