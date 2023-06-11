5f1654ff9a62e875d288f0732745c6a6c95dd00e
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-06-11 01:52:26 -0400

    Add removeFromCart

--- src/game-controller.js
+++ src/game-controller.js
@@ -1,34 +1,37 @@
 const falso = require("@ngneat/falso");
-const { nanoid } = require( "nanoid" );
-const games = require( "../data/games.json" );
-const { genreMatch, consoles, gameSystem } = require("../data/gameGenreAndPlatfromDatabase")
+const { nanoid } = require("nanoid");
+const games = require("../data/games.json");
+const {
+  genreMatch,
+  consoles,
+  gameSystem,
+} = require("../data/gameGenreAndPlatfromDatabase");
 
-function create( games, gameName )
-{
-	const game = {
-	"id": nanoid(4),
-	"name": gameName,
-	"genre": genreMatch(),
-	"platform": gameSystem( consoles ),
-	"priceInCents": falso.randFloat({min: 10, max: 60, fraction: 2}) * 100
-	};
+const inform = console.log;
+
+function create(games, gameName) {
+  const game = {
+    id: nanoid(4),
+    name: gameName,
+    genre: genreMatch(),
+    platform: gameSystem(consoles),
+    priceInCents: falso.randFloat({ min: 10, max: 60, fraction: 2 }) * 100,
+  };
   games.push(game);
   return games;
 }
 
 function index(games) {
-	return games.map((game) => `id: ${game.id} Name: ${game.name}`);
-};
+  return games.map((game) => `id: ${game.id} Name: ${game.name}`);
+}
 
 function show(games, gameId) {
-	let game = games.find( ( game ) => game.id === gameId );
-	let immersiveView = Object.assign({}, game)
-	return immersiveView
-};
-
+  let game = games.find((game) => game.id === gameId);
+  let immersiveView = Object.assign({}, game);
+  return immersiveView;
+}
 
 function destroy(games, gameId) {
-const inform = console.log;
   const index = games.findIndex((game) => game.id === gameId);
   if (index > -1) {
     games.splice(index, 1);
@@ -40,10 +43,8 @@ const inform = console.log;
   }
 }
 
-function update ( games, gameId, gamePlatform )
-{
-const inform = console.log;
-const index = games.findIndex((game) => game.id === gameId);
+function update(games, gameId, gamePlatform) {
+  const index = games.findIndex((game) => game.id === gameId);
   if (index > -1) {
     games[index].id = gameId;
     games[index].platform = gamePlatform;
@@ -54,21 +55,16 @@ const index = games.findIndex((game) => game.id === gameId);
     return games;
   }
 }
-e
-function addToCart(games, gameName, cart  ) {
-	const inform = console.log;
 
-
-}
-
-
-
+function addToCart ( games, gameName, cart ) { }
+function removeFromCart ( games, gameName, cart,)
 
 module.exports = {
-	create,
-	index,
-	show,
-	destroy,
-	update,
-	addToCart,
+  create,
+  index,
+  show,
+  destroy,
+  update,
+  addToCart,
+  removeFromCart,
 };
